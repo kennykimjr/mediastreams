0b64974cc2b51775d11f0002ab156ceb1782e558
kennykimjr <kennykimjr@gmail.com>
2019-01-12 01:36:06 -0800

    Now renders a list of tweets on a regular poll.

--- src/feeds.js
+++ src/feeds.js
@@ -1,12 +1,32 @@
 import React, { Component} from "react";
+import { Image, List } from 'semantic-ui-react'
 
 class Feeds extends Component{
   constructor(props) {
     super(props)
+    this.renderTweets = this.renderTweets.bind(this)
+  }
+  renderTweets(tweets) {
+    const items = []
+    Object.keys(tweets).forEach((tweet) => {
+      items.push(
+        <List.Item key={tweet}>
+          <Image avatar src={tweets[tweet].user.profile_image_url}/>
+          <List.Content>
+            <List.Header>{tweets[tweet].user.name}</List.Header>
+            {tweets[tweet].text}
+          </List.Content>
+        </List.Item>
+      )
+    })
+    return items
   }
   render(){
+    const items = this.renderTweets(this.props.tweets)
     return(
-      <div>test</div>
+      <List celled>
+        {items}
+      </List>
     );
   }
 }
