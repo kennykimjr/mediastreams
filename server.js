10ed290f9cf0cae67b5fd99fa6a36855f378a966
kennykimjr <kennykimjr@gmail.com>
2019-01-12 13:39:07 -0800

    A New search now clears the table and re-renders a new set of tweets. Tweets are now displayed with their creation dates.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const staticMiddleware = express.static(publicPath)
 app.get('/tweets', (req, res) => {
   const options = {
     url: `https://api.twitter.com/1.1/search/tweets.json?q=%23${req.query.term}&since_id=${req.query.since}`,
-    headers: { 'Authorization': `Bearer ${process.env.KEY}` }
+    headers: { 'Authorization': `Bearer ${process.env.TWITTER_KEY}` }
   }
   request(options, (err, response, body) => {
     if (err) throw err;
--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import React, { Component} from "react";
 import Feeds from './feeds'
-import { Form, Input } from 'semantic-ui-react'
+import { Form, Input, Segment } from 'semantic-ui-react'
 
 let twitterInterval = null
 
 class App extends Component{
   constructor(props) {
     super(props)
-    this.state = { tweets: {}, maxTwitterId: '' }
+    this.state = { tweets: {}, maxTwitterId: '', term: '' }
     this.search = this.search.bind(this)
     this.handleTweets = this.handleTweets.bind(this)
   }
@@ -18,29 +18,34 @@ class App extends Component{
     twitterInterval = window.setInterval(() => {
       fetch(`/tweets?term=${value}&since=${this.state.maxTwitterId}`)
       .then(response => response.json())
-      .then(result => this.handleTweets(result))
+      .then(result => this.handleTweets(result, value))
     },
     3000)
   }
-  handleTweets(tweets) {
-    if (tweets.search_metadata.max_id_str != this.state.maxTwitterId) {
+  handleTweets(tweets, value) {
+    const newMax = tweets.search_metadata.max_id_str
+    if (newMax != this.state.maxTwitterId) {
       const recievedTweets = {}
+      const currentTweets = value == this.state.term ? this.state.tweets : {}
       tweets.statuses.forEach((tweet) => {
         recievedTweets[tweet.id] = tweet
       })
       this.setState({
-        maxTwitterId: tweets.search_metadata.max_id_str,
-        tweets: Object.assign(recievedTweets, this.state.tweets)
+        maxTwitterId: value == this.state.term ? newMax : '',
+        tweets: Object.assign(recievedTweets, currentTweets),
+        term: value
       })
     }
   }
   render(){
     const newProps = {tweets: this.state.tweets, grams: this.state.grams}
     return(
-      <Form onSubmit={this.search}>
-        <Input placeholder='Search for a hashtag!'/>
-        <Feeds {...newProps}/>
-      </Form>
+        <Form onSubmit={this.search}>
+          <Segment inverted>
+            <Input placeholder='Search for a hashtag!' icon="search" inverted/>
+          </Segment>
+          <Feeds {...newProps}/>
+        </Form>
     )
   }
 }
